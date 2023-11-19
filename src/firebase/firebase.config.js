b08e6903f5a863c8f5340a0948eb6d6cf9a72c39
Shadipto Mojumder <118707478+shadipto58@users.noreply.github.com>
2023-11-19 13:40:10 +0600

    Update firebase.config.js 2.0

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -9,7 +9,14 @@ const firebaseConfig = {
     messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
     appId: import.meta.env.VITE_APPID
 };
-
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDjg0KtitJ8otEGMVtRow-BTT8euoONVDY",
+//   authDomain: "careos-7e27e.firebaseapp.com",
+//   projectId: "careos-7e27e",
+//   storageBucket: "careos-7e27e.appspot.com",
+//   messagingSenderId: "256143795358",
+//   appId: "1:256143795358:web:2393d5eeccc45ab306478f"
+// };
 const app = initializeApp(firebaseConfig);
 
 export default app;
